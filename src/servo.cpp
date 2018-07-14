b8b8c52d98eece9a3b2401eff1413fc56889b435
Will <quarkswithforks@gmail.com>
2018-07-14 14:26:21 -0600

    wip to move logic to func

--- src/servo.cpp
+++ src/servo.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 
 #define NUM_GPIO 32
+#define MIN_RANGE 500
+#define MAX_RANGE 2500
 
 int run = 1;
 int gpio_pin;
@@ -15,41 +17,71 @@ void stop(int signum)
 	run = 0;
 }
 
+int rotate_servo(int last_pos, int new_pos)
+{
+	if (new_pos > MAX_RANGE || new_pos < MIN_RANGE)
+	{
+		std::cout "Position of " << new_pos << " is invalid. Please select a range between " << MIN_RANGE << " and " << MAX_RANGE << ".\n";
+		return last_pos;
+	}
+	return 0;
+	int pos = last_pos;
+	int step = 1;
+	if (new_pos < pos)
+	{
+		step = -1;
+	}
+	while (pos != new_pos)
+	{
+		printf("%d \n", pos);
+
+		gpioServo(gpio_pin, pos);
+		pos += step;
+		time_sleep(0.001);
+	}
+	return new_pos;
+}
+
 int main(int argc, char *argv[])
 {
-	if (gpioInitialise() < 0) return -1;
+	int last_pos = 1500;
+	int new_pos = last_pos;
+
+	if (gpioInitialise() < 0)
+	{
+		std::cout << "Error initializing gpio - exiting\n";
+		return -1;
+	}
 
 	gpioSetSignalFunc(SIGINT, stop);
 
-	if (argc == 1) gpio_pin = 4;
-	else
+	if (argc == 2)
 	{
 		gpio_pin = atoi(argv[1]);
 	}
 
-	printf(", control C to stop.\n");
-	int last_pos = 1500;
-	int new_pos = last_pos;
+	printf("Calibration routine starting - control C to stop.\n");
 	while (run)
 	{
 		std::cout << "new pos: " << new_pos << " last pos " << last_pos << std::endl;
 		if (new_pos != last_pos)
 		{
-			int pos = last_pos;
-			int step = 1;
-			if (new_pos < pos)
-			{
-				step = -1;
-			}
-			while (pos != new_pos)
-			{
-				printf("%d \n", pos);
+			last_pos = rotate_servo(last_pos, new_pos);
+			//int pos = last_pos;
+			//int step = 1;
+			//if (new_pos < pos)
+			//{
+			//	step = -1;
+			//}
+			//while (pos != new_pos)
+			//{
+			//	printf("%d \n", pos);
 
-				gpioServo(gpio_pin, pos);
-				pos += step;
-				time_sleep(0.001);
-			}
-			last_pos = pos;
+			//	gpioServo(gpio_pin, pos);
+			//	pos += step;
+			//	time_sleep(0.001);
+			//}
+			//last_pos = pos;
 		}
 
 		std::cout << "New position? :\n";
