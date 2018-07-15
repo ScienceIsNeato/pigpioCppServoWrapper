9cd06ad441285a40df4574f47fae78aa82f47e10
Will Martin <quarkswithforks@gmail.com>
2018-07-15 03:35:22 +0000

    adding template for tester

---
+++ src/servo_tester.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include <pigpio.h>
+#include <iostream>
+#include "../include/pigpioServo.h"
+
+
+int main(int argc, char *argv[])
+{
+	pigpioServo servo = new pigpioServo();
+	return 0;
+}
+
