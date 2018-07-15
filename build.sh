e1d1f1b53c1daf415345242dab060361e2afe3bc
Will Martin <quarkswithforks@gmail.com>
2018-07-15 03:43:24 +0000

    adding debug output to build script

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
+echo "Building calibration binary..."
 g++ -Wall -pthread -o calibrate src/calibrate.cpp -lpigpio -lrt
+echo "Successfully built 'calibrate'"
+
+echo "Building test binary..."
 g++ -Wall -pthread -o servo_tester src/pigpioServo.cpp src/servo_tester.cpp -lpigpio -lrt
+echo "Successfully built 'servo_tester'"
