7df2d3e9fd198d6ad8e155e3ba86e1a6d0f65e56
Will <quarkswithforks@gmail.com>
2018-07-14 21:24:07 -0600

    boilerplate for servo class

---
+++ include/pigpioServo.h
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include <pigpio.h>
+#include <iostream>
+
+#define NUM_GPIO 32
+#define MIN_PULSE_WIDTH 500
+#define MAX_PULSE_WIDTH 2500
+#define DEFAULT_CENTER 1500
+
+// Used when initializing the object if there is a distance or angle offset
+// between the servo and the sensor point
+struct InitialOffset
+{
+	double offsetX; // x distance between servo and sensor
+	double offsetY; // y distance between servo and sensor
+	double offsetAngle; // angle offset between servo and sensor
+};
+
+// A way to map an angle in degrees to a pulsewidth
+struct AngleMap
+{
+	double angle;
+	int pulse_width;
+};
+
+class pigpioServo
+{
+public:
+	pigpioServo();
+	pigpioServo(AngleMap max_left, AngleMap center, AngleMap max_right);
+	pigpioServo(AngleMap max_left, AngleMap center, AngleMap max_right, InitialOffset initial_offset);
+	~pigpioServo();
+	void Initialize();
+	void Stop();
+	void TurnToAngle(double angle);
+	bool IsAngleValid(double angle);
+
+private:
+	double _x_offset;
+	double _y_offset;
+	double _angle_offset;
+	int _gpio_pin;
+	AngleMap _max_left;
+	AngleMap _center;
+	AngleMap _max_right;
+	int default_pulsewidth;
+	int current_pulsewidth;
+
+	int AngleToPulseWidth(double angle);
+};
---
+++ src/pigpioServo.cpp
@@ -0,0 +1,46 @@
+#include "pigpioServo.h"
+
+void pigpioServo::pigpioServo()
+{
+
+}
+
+void pigpioServo::pigpioServo(AngleMap max_left, AngleMap center, AngleMap max_right)
+{
+
+}
+
+void pigpioServo::pigpioServo(AngleMap max_left, AngleMap center, AngleMap max_right, InitialOffset initial_offset)
+{
+
+}
+
+void pigpioServo::~pigpioServo()
+{
+
+}
+
+void pigpioServo::Initialize()
+{
+
+}
+
+void pigpioServo::Stop()
+{
+
+}
+
+void pigpioServo::TurnToAngle(double angle)
+{
+
+}
+
+bool pigpioServo::IsAngleValid(double angle)
+{
+
+}
+
+int pigpioServo::AngleToPulseWidth(double angle)
+{
+
+}
