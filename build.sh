a6dae4136dc6f7f36279d0afbd1a6175204f75d9
ScienceIsNeato <wmartin@sorensonmedia.com>
2018-07-16 15:40:30 -0600

    adding error handling to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 echo "Building calibration binary..."
 g++ -Wall -pthread -o calibrate src/calibrate.cpp -lpigpio -lrt
-echo "Successfully built 'calibrate'"
+if [ $? -eq 0 ]; then
+    echo "Successfully built 'calibrate'"
+else
+    echo "ERROR - Failed to build 'calibrate'"
+    exit 1
+fi
 
 echo "Building test binary..."
 g++ -Wall -pthread -o servo_tester src/pigpioServo.cpp src/servo_tester.cpp -lpigpio -lrt
-echo "Successfully built 'servo_tester'"
+
+if [ $? -eq 0 ]; then
+    echo "Successfully built 'servo_tester'"
+else
+    echo "ERROR - Failed to build 'servo_tester'"
+    exit 1
+fi
+
